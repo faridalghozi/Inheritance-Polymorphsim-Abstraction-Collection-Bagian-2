1bb96b46132659e69571c59c0f8a1fc27584bd71
faridalghozi <faridalghozi@gmail.com>
2020-07-01 20:20:51 +0700

    Bagian #2

---
+++ KaryawanHarian.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TugasLab9
+{
+    public class KaryawanHarian : Karyawan
+    {
+        public double UpahPerJam;
+        public double JumlahJamKerja;
+        public override double Gaji()
+        {
+            return this.UpahPerJam * this.JumlahJamKerja;
+        }
+    }
+}
---
+++ KaryawanTetap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TugasLab9
+{
+    public class KaryawanTetap : Karyawan
+    {
+        public double GajiBulanan;
+        public override double Gaji()
+        {
+            return this.GajiBulanan;
+        }
+    }
+}
